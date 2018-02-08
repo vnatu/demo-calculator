01713562c75dad42f6a8c322d25fac71dbba3df0
Vaibhav Natu <vnatu@users.noreply.github.com>
2018-02-08 18:58:58 +0530

    initial commit

---
+++ src/main/java/com/cglx/Calculator.java
@@ -0,0 +1,27 @@
+package com.cglx;
+
+public class Calculator {
+
+    public Calculator() {
+    }
+
+    public int add(int a, int b)
+    {
+        return a+b;
+    }
+
+    public int multiply(int a, int b)
+    {
+        return a*b;
+    }
+
+    public  int divide(int a, int b)
+    {
+        return a/b;
+    }
+
+    public int substract(int a, int b)
+    {
+        return a - b;
+    }
+}
